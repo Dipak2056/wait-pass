8051bc122ce00ebd045ca6aeaf85e31669008b05
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-20 16:07:04 +1000

    writing all the component in the single page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <div className="header-container">
+        <h1>WAIT PASS</h1>
+        <div className="menu-button"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
