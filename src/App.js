2dcfa12d6be81a1e47e5b5e570850c41f68753a4
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-20 17:03:10 +1000

    creating some design lay-out

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ function App() {
     <div className="wrapper">
       <div className="header-container">
         <h1>WAIT PASS</h1>
-        <div className="menu-button"></div>
+        <div className="menu-button">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+      </div>
+      <div className="main">
+        <h3>All-item-Menu</h3>
+        <div className="course">
+          <h4>start-up</h4>
+        </div>
+        <div className="course">
+          <h4>entree</h4>
+        </div>
+        <div className="course">
+          <h4>Main</h4>
+        </div>
+        <div className="course">
+          <h4>Desser</h4>
+        </div>
+        <div className="course">
+          <h4>Drinks</h4>
+        </div>
       </div>
     </div>
   );
